530c5ca2c5b00e3a265fee87a5e84c107554e6c8
Edgard de Haro <edgardha@gmail.com>
2018-12-30 22:31:51 +0100

    call strcpy from another function

--- vuln.cc
+++ vuln.cc
@@ -2,15 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+int copyAndPrint(char *name){
+    char buff[128];
+
+    strcpy(buff, name);
+    printf("Your name is %s\n", buff);
+
+    return 0;
+}
+
 int main(int argc, char *argv[]) {
     char buff[128];
 
-    if (argc != 2)
+    if (argc != 2) {
+        printf("Enter your name as argument. Ex: ./<prog_name> pepe\n");
         return 1;
+    }
 
-    strcpy(buff, argv[1]);
-
-    printf("Your name is %s\n", buff);
+    copyAndPrint(argv[1]);
 
     return 0;
     
